b15d4aa91f02b0750498c677559073c1b331261c
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-08-30 09:57:54 +0330

    dark theme , add schedule

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from datetime import datetime, timedelta, timezone
 import pytz
 from Variable import *
+import requests
+import schedule
+import time
+
+plt.style.use('dark_background')
 
 API_URL = f'https://api.hetzner.cloud/v1/servers/{SERVER_ID}/metrics'
 headers = {
@@ -30,44 +36,70 @@ def get_network_traffic(start_time, end_time, step):
     return data['metrics']
 
 def plot_and_save_traffic(timestamps, inbound, outbound):
-    plt.figure(figsize=(12, 6))
-    plt.plot(timestamps, inbound, label='Inbound Traffic (MBps)')
-    plt.plot(timestamps, outbound, label='Outbound Traffic (MBps)')
-    plt.xlabel('Time')
-    plt.ylabel('Traffic (MBps)')
-    plt.title('Network Traffic over the Last 24 Hours')
-    plt.legend()
+    plt.figure(figsize=(12, 6))  # اندازه بزرگ‌تر برای وضوح بیشتر
+
+    # رسم خطوط
+    plt.plot(timestamps, inbound, label='Inbound Traffic (MB)', color='#1f77b4', linewidth=2)
+    plt.plot(timestamps, outbound, label='Outbound Traffic (MB)', color='#ff7f0e', linewidth=2)
+
+    # تنظیمات محورهای گراف
+    plt.xlabel('Time', fontsize=14, color='white')
+    plt.ylabel('Traffic (MB)', fontsize=14, color='white')
+    plt.title(TITLE, fontsize=16, color='white')
+
+    plt.grid(True, linestyle='--', color='gray', alpha=0.7)
+    
+    plt.legend(loc='upper right', fontsize=12)
     plt.grid(True)
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig('network_traffic.png')
+    plt.savefig('network_traffic.png', bbox_inches='tight', dpi=300)
     plt.show()
 
 def convert_to_tehran_time(utc_dt):
     tehran_tz = pytz.timezone('Asia/Tehran')
     return utc_dt.astimezone(tehran_tz)
 
-end_time = datetime.now(timezone.utc)
-start_time = end_time - timedelta(days=1)
+def start ():
+    end_time = datetime.now(timezone.utc)
+    start_time = end_time - timedelta(days=7)
 
-step = 3600
-traffic_data = get_network_traffic(start_time, end_time, step)
+    step = 3600
+    traffic_data = get_network_traffic(start_time, end_time, step)
 
-timestamps = []
-inbound_traffic = []
-outbound_traffic = []
+    timestamps = []
+    inbound_traffic = []
+    outbound_traffic = []
 
-time_series = traffic_data['time_series']
-for i in range(0, len(time_series['network.0.bandwidth.in']['values'])):
-    utc_timestamp = datetime.fromtimestamp(time_series['network.0.bandwidth.in']['values'][i][0], timezone.utc)
-    tehran_timestamp = convert_to_tehran_time(utc_timestamp)
-    
-    inbound = float(time_series['network.0.bandwidth.in']['values'][i][1]) / (1024 * 1024)
-    outbound = float(time_series['network.0.bandwidth.out']['values'][i][1]) / (1024 * 1024)
-    
-    timestamps.append(tehran_timestamp)
-    inbound_traffic.append(inbound)
-    outbound_traffic.append(outbound)
+    time_series = traffic_data['time_series']
+    for i in range(0, len(time_series['network.0.bandwidth.in']['values'])):
+        utc_timestamp = datetime.fromtimestamp(time_series['network.0.bandwidth.in']['values'][i][0], timezone.utc)
+        tehran_timestamp = convert_to_tehran_time(utc_timestamp)
+        
+        inbound = float(time_series['network.0.bandwidth.in']['values'][i][1]) / (1024 * 1024)
+        outbound = float(time_series['network.0.bandwidth.out']['values'][i][1]) / (1024 * 1024)
+        
+        timestamps.append(tehran_timestamp)
+        inbound_traffic.append(inbound)
+        outbound_traffic.append(outbound)
 
-# create graph
-plot_and_save_traffic(timestamps, inbound_traffic, outbound_traffic)
+    plot_and_save_traffic(timestamps, inbound_traffic, outbound_traffic)
+
+    photo_path = './network_traffic.png'
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+
+    with open(photo_path, 'rb') as photo:
+        response = requests.post(url, data={'chat_id': CHANNEL_ID, 'caption': CAPTION}, files={'photo': photo})
+
+    if response.status_code == 200:
+        print("Photo sent successfully")
+    else:
+        print("Failed to send photo:", response.text)
+
+
+schedule.every().friday.at(TIME , pytz.timezone('Asia/Tehran')).do(start)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
